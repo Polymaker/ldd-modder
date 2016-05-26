a657cac573203e0efe5da53e5b0eadb3bf43ca8f
James Turner <james_w_turner27@hotmail.com>
2016-05-26 16:25:16 -0400

    added DecorationMapping class + a little documentation

---
+++ LDDModder/Library/LDD/DecorationMapping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LDDModder.LDD
+{
+    public class DecorationMapping
+    {
+        public int DecorationID { get; set; }
+
+        public int DesignID { get; set; }
+
+        public int SurfaceID { get; set; }
+    }
+}
